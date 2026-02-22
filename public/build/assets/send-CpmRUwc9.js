d98e1df2aa898aad1d41cd33ad9b60407dd1e15c
FRANCIS KIOKO <57718524+frajosan97@users.noreply.github.com>
2026-02-22 10:10:35 +0300

    Edited

---
+++ public/build/assets/send-CpmRUwc9.js
@@ -0,0 +1,11 @@
+import{u as p,n as u,p as h,q as i,c as l}from"./AppLayout-Clmahtql.js";import{r as n,j as t}from"./app-DFqvjIwK.js";const s=n.forwardRef(({className:e,bsPrefix:o,as:a="span",...c},r)=>(o=p(o,"input-group-text"),t.jsx(a,{ref:r,className:u(e,o),...c})));s.displayName="InputGroupText";const j=e=>t.jsx(s,{children:t.jsx(i,{type:"checkbox",...e})}),k=e=>t.jsx(s,{children:t.jsx(i,{type:"radio",...e})}),x=n.forwardRef(({bsPrefix:e,size:o,hasValidation:a,className:c,as:r="div",...d},y)=>{e=p(e,"input-group");const m=n.useMemo(()=>({}),[]);return t.jsx(h.Provider,{value:m,children:t.jsx(r,{ref:y,...d,className:u(c,e,o&&`${e}-${o}`,a&&"has-validation")})})});x.displayName="InputGroup";const v=Object.assign(x,{Text:s,Radio:k,Checkbox:j});/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const I=[["circle",{cx:"12",cy:"12",r:"1",key:"41hilf"}],["circle",{cx:"12",cy:"5",r:"1",key:"gxeob9"}],["circle",{cx:"12",cy:"19",r:"1",key:"lyex9k"}]],R=l("ellipsis-vertical",I);/**
+ * @license lucide-react v0.562.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const f=[["path",{d:"M14.536 21.686a.5.5 0 0 0 .937-.024l6.5-19a.496.496 0 0 0-.635-.635l-19 6.5a.5.5 0 0 0-.024.937l7.93 3.18a2 2 0 0 1 1.112 1.11z",key:"1ffxy3"}],["path",{d:"m21.854 2.147-10.94 10.939",key:"12cjpa"}]],g=l("send",f);export{R as E,v as I,g as S};
--- resources/js/Components/Common/ApplicationLogo.jsx
+++ resources/js/Components/Common/ApplicationLogo.jsx
@@ -3,13 +3,9 @@ import { Image } from "react-bootstrap";
 export default function ApplicationLogo(props) {
     return (
         <Image
-            src="/storage/images/logos/logo.png"
+            src="https://i.ibb.co/35QhMPTZ/raha-tele.png"
             alt="Logo"
             {...props}
-            onError={(e) => {
-                e.target.onerror = null; // Prevents infinite loop if the image fails to load
-                e.target.src = "/images/logo-fallback.png"; // Fallback image
-            }}
             onLoad={(e) => {
                 e.target.style.display = "block"; // Ensure the image is displayed after loading
             }}
